a0cbfed3ace353982a87840ef5738d308fea5ad6
Michael Adams <madams6216@gmail.com>
2015-08-11 10:43:28 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root 'home#index'
 
-  get 'home/convert'
+  get 'search' =>'home#convert'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

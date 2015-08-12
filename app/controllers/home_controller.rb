5a9fa564079b02d1d255b9a33f848ec547f895d5
Michael Adams <madams6216@gmail.com>
2015-08-12 11:19:33 -0400

    all modified work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,25 @@ class HomeController < ApplicationController
   end
 
   def convert
+
+  	fahrenheit = params[:fahrenheit]
+
+  	fahrenheit = fahrenheit.to_f
+
+  	celsius = convert_to_celsius(fahrenheit)
+
+  	celsius = celsius.round
+
+  	message = "the celsius result is #{celsius}"
+
+  	@message = message
+
   end
+
+   def convert_to_celsius(fahrenheit)
+
+  		celsius = (fahrenheit - 32) * 5.0 / 9.0
+
+  		return celsius
+  	end
 end
